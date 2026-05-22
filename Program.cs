ed947b2c959b94ad0aeaa3707c93e38bdf107691
dev <Jorsg>
2026-05-21 22:27:00 -0400

    Add package Functions.Work and variables to local.setting.js

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -5,10 +7,16 @@ using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
+var credential = new DefaultAzureCredential();
+// Initialize SecretClient. Ensure package Azure.Security.KeyVault.Secrets is referenced
+// and replace <your-key-vault-name> with your Key Vault name.
+//var client = new SecretClient(new Uri("https://<your-key-vault-name>.vault.azure.net/"), credential);
+
 builder.ConfigureFunctionsWebApplication();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+  
 
 builder.Build().Run();
